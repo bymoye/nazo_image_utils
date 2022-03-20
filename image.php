f9087430ade7a93469c33f212194f0c054f01cf1
bymoye <964907582@qq.com>
2022-03-20 20:35:15 +0800

    init

--- image.php
+++ image.php
@@ -3,11 +3,10 @@
  * @Author: bymoye
  * @Date:   2022-03-20 18:47:30
  * @Last Modified by:   bymoye
- * @Last Modified time: 2022-03-20 20:18:10
+ * @Last Modified time: 2022-03-20 20:35:00
  */
 $json_string = file_get_contents('manifest.json');
 $data = json_decode($json_string,True);
 $keys = array_keys($data);
 header('Content-Type: image/webp');
-// 这里写点东西
 readfile('webp/'.$keys[array_rand($keys)].'.source.webp');
